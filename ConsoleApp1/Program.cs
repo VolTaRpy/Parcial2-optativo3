d7abf5f88e11467e1c51d1a1d7430f8a4cbf95dc
Matias Martinez <matias_wicho@hotmail.com>
2024-05-22 10:57:10 -0400

    examen parcial 2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using Services.Logica;
+using Repository.Personas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+string connectionString = "Host=localhost;port=5432;database=Store;Username=postgres;Password=matias;";
+PersonaService personaService = new PersonaService(connectionString);
+
+Console.WriteLine("Ingrese: \n a - para insertar \n b - para listar");
+string opcion = Console.ReadLine();
+
+if (opcion == "a")
+{
+    personaService.add(new PersonaModel
+    {
+        id_banco = 1,
+        nombre = "Matias",
+        apellido = "Martinez",
+        documento = "4185539",
+        direccion = "San Martin 136 c/ P. J. Caballero",
+        mail = "matias_wicho@hotmail.com",
+        celular = 0985138981,
+        estado = "Activo"
+    });
+}
+
+if (opcion == "b")
+{
+    personaService.GetAll().ToList().ForEach(persona =>
+    Console.WriteLine(
+        $"id_banco: {persona.id_banco} \n" +
+        $"Nombre: {persona.nombre} \n" +
+        $"Apellido: {persona.apellido} \n" +
+        $"Documento: {persona.documento} \n" +
+        $"Direccion: {persona.direccion} \n" +
+        $"Email: {persona.mail} \n" +
+        $"Celular: {persona.celular} \n" +
+        $"Estado: {persona.estado} \n"
+        )
+    );
+}
---
+++ services/Logica/SucursalService.cs
@@ -0,0 +1,47 @@
+﻿using Repository.Sucursal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Logica
+{
+    public class SucursalService : ISucursalRepository
+    {
+        private SucursalRepository sucursalRepository;
+        public SucursalService (string connectionString)
+        {
+            sucursalRepository = new SucursalRepository (connectionString);
+        }
+
+        public bool add(SucursalModel sucursalModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SucursalModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool update(SucursalModel sucursalModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool validarDatos(SucursalModel sucursal)
+        {
+            if (sucursal == null)
+                return false;
+            if (string.IsNullOrEmpty(sucursal.direccion) || sucursal.direccion.Length<9)
+                return false;
+
+        }
+    }
+}
